b0d09d68ed6e4078ee580e62188b6be21b6a1bde
VZhyrytskiy <Vitaliy_Zhyrytskyy@epam.com>
2023-05-15 18:02:20 +0300

    task2-comments

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -7,6 +7,7 @@ import { ProductsMock } from '../mocks/product.mock';
 })
 export class ProductsService {
   getProducts(): Array<ProductModel> {
-    return JSON.parse(JSON.stringify(ProductsMock));
+    // return JSON.parse(JSON.stringify(ProductsMock));
+    return ProductsMock;
   }
 }
